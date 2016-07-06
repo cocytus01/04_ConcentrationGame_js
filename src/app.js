c7230c5bd5a92ef9622d3bf04171df508c07ab78
junpei yamaguchi <yamaguchi.cocytos@gmail.com>
2016-07-06 10:38:17 +0900

    格子状にカードを配置

--- src/app.js
+++ src/app.js
@@ -13,17 +13,11 @@ var game = cc.Layer.extend({
         this._super();
         var size = cc.director.getWinSize();
 
-      for(var j = 0; j < 4 ; j++){
-      for(var i = 0; i < 4; i++) {
-        this.sprite = new cc.Sprite(res.cover_png);
-        this.sprite.attr({
-                x: size.width * 0.2+70 * i ,
-                y: size.height * 0.2+70 * j,
-                scale: 1,
-                rotation: 0
-        });
-        this.addChild(this.sprite, 0);
-      }
-    }
-  },
+        for (i = 0; i < 16; i++) {
+            var tile = cc.Sprite.create(res.cover_png);
+            this.addChild(tile, 0);
+            //タイルを格子状に配置する計算式
+            tile.setPosition(49 + i % 4 * 74, 400 - Math.floor(i / 4) * 74);
+        }
+    },
 });
